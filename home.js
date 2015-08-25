305154eb49850f317f0cb0bc64f886ece43cbcf5
Noah Thomas <noahbthomas@sandiego.edu>
2015-08-25 12:18:47 -0700

    About time! The last commit worked. Using full database code now (fingers crossed)

--- home.js
+++ home.js
@@ -1,33 +1,30 @@
-document.addEventListener('deviceready', function(){
-	alert("HI")
-}, false);
 
-// document.addEventListener('deviceready', function() {
-// 	alert("Document is ready")
-// 	var output = $('#output')
+document.addEventListener('deviceready', function() {
+	alert("Document is ready")
+	var output = $('#output')
 
-// 	$.ajax({
-// 		url: 'http://noahthomas.us/pg/landmarks.php',
-// 		dataType: 'jsonp',
-// 		jsonp: 'jsoncallback',
-// 		timeout: 5000,
-// 		success: function(data, status) {
-// 			$.each(data, function(i, item) {
-// 				var landmark = '<h1>' + item.name + '</h1>'
-// 				+ '<p>' + item.latitude + '<br>'
-// 				+ item.longitude + '</p>'
+	$.ajax({
+		url: 'http://noahthomas.us/pg/landmarks.php',
+		dataType: 'jsonp',
+		jsonp: 'jsoncallback',
+		timeout: 5000,
+		success: function(data, status) {
+			$.each(data, function(i, item) {
+				var landmark = '<h1>' + item.name + '</h1>'
+				+ '<p>' + item.latitude + '<br>'
+				+ item.longitude + '</p>'
 
-// 				output.append(landmark)
-// 			})
-// 		},
-// 		error: function() {
-// 			output.text('There was an error loading the data.')
-// 		}
-// 	})
+				output.append(landmark)
+			})
+		},
+		error: function() {
+			output.text('There was an error loading the data.')
+		}
+	})
 
-// 	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
-// 	$("h3").html("HELLO ALIENS")
-// }, false)
+	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
+	$("h3").html("HELLO ALIENS")
+}, false)
 
 
 
