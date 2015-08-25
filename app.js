0f30057c4320774208c38369c49efa9feae7ebe7
Noah Thomas <noahbthomas@sandiego.edu>
2015-08-25 12:07:48 -0700

    Only alert in js file

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-document.addEventListener('deviceready', function() {
-	alert("Document is ready")
-	var output = $('#output')
+alert("HIIIII")
 
-	$.ajax({
-		url: 'http://noahthomas.us/pg/landmarks.php',
-		dataType: 'jsonp',
-		jsonp: 'jsoncallback',
-		timeout: 5000,
-		success: function(data, status) {
-			$.each(data, function(i, item) {
-				var landmark = '<h1>' + item.name + '</h1>'
-				+ '<p>' + item.latitude + '<br>'
-				+ item.longitude + '</p>'
+// document.addEventListener('deviceready', function() {
+// 	alert("Document is ready")
+// 	var output = $('#output')
 
-				output.append(landmark)
-			})
-		},
-		error: function() {
-			output.text('There was an error loading the data.')
-		}
-	})
+// 	$.ajax({
+// 		url: 'http://noahthomas.us/pg/landmarks.php',
+// 		dataType: 'jsonp',
+// 		jsonp: 'jsoncallback',
+// 		timeout: 5000,
+// 		success: function(data, status) {
+// 			$.each(data, function(i, item) {
+// 				var landmark = '<h1>' + item.name + '</h1>'
+// 				+ '<p>' + item.latitude + '<br>'
+// 				+ item.longitude + '</p>'
 
-	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
-	$("h3").html("HELLO ALIENS")
-}, false)
+// 				output.append(landmark)
+// 			})
+// 		},
+// 		error: function() {
+// 			output.text('There was an error loading the data.')
+// 		}
+// 	})
+
+// 	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
+// 	$("h3").html("HELLO ALIENS")
+// }, false)
 
 
 
