da2d282f92ab14aca88bdbf72624f41783de803d
Noah Thomas <noahbthomas@sandiego.edu>
2015-08-25 10:51:50 -0700

    Changed js file to have device ready

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+document.addEventListener('deviceready', function() {
 	var output = $('#output')
 
 	$.ajax({
@@ -20,3 +20,26 @@ $(document).ready(function() {
 		}
 	})
 })
+
+// $(document).ready(function() {
+// 	var output = $('#output')
+
+// 	$.ajax({
+// 		url: 'http://noahthomas.us/pg/landmarks.php',
+// 		dataType: 'jsonp',
+// 		jsonp: 'jsoncallback',
+// 		timeout: 5000,
+// 		success: function(data, status) {
+// 			$.each(data, function(i, item) {
+// 				var landmark = '<h1>' + item.name + '</h1>'
+// 				+ '<p>' + item.latitude + '<br>'
+// 				+ item.longitude + '</p>'
+
+// 				output.append(landmark)
+// 			})
+// 		},
+// 		error: function() {
+// 			output.text('There was an error loading the data.')
+// 		}
+// 	})
+// })
