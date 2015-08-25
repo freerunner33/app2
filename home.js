279df3ffbbbf1e868cad206248ba2f650f73bb61
Noah Thomas <noahbthomas@sandiego.edu>
2015-08-25 15:39:40 -0700

    Changed database stuff to menu_1

--- home.js
+++ home.js
@@ -1,20 +1,24 @@
 
 document.addEventListener('deviceready', function() {
-	alert("You look funny")
+	alert("Your mom")
 	var output = $('#output')
 
 	$.ajax({
-		url: 'http://noahthomas.us/pg/landmarks.php',
+		url: 'http://noahthomas.us/USD/menu.php',
 		dataType: 'jsonp',
 		jsonp: 'jsoncallback',
-		timeout: 5000,
+		timeout: 1000,
 		success: function(data, status) {
 			$.each(data, function(i, item) {
-				var landmark = '<h1>' + item.name + '</h1>'
-				+ '<p>' + item.latitude + '<br>'
-				+ item.longitude + '</p>'
+				var menuitem = '<h1>' + item.name + '</h1>'
+				+ '<h3>' + item.category + '</h3>'
+				+ '<p>Price1: ' + item.price1 + '<br>'
+				+ 'Price2: ' + item.price2 + '<br>' 
+				+ 'Price3: ' + item.price3 + '<br>'
+				+ 'Price4: ' + item.price4 + '<br>'
+				+ 'Price5: ' + item.price5 + '</p>'
 
-				output.append(landmark)
+				output.append(menuitem)
 			})
 		},
 		error: function() {
@@ -25,6 +29,33 @@ document.addEventListener('deviceready', function() {
 	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
 	$("h3").html("HELLO ALIENS")
 }, false)
+
+// document.addEventListener('deviceready', function() {
+// 	alert("You look funny")
+// 	var output = $('#output')
+
+// 	$.ajax({
+// 		url: 'http://noahthomas.us/pg/landmarks.php',
+// 		dataType: 'jsonp',
+// 		jsonp: 'jsoncallback',
+// 		timeout: 5000,
+// 		success: function(data, status) {
+// 			$.each(data, function(i, item) {
+// 				var landmark = '<h1>' + item.name + '</h1>'
+// 				+ '<p>' + item.latitude + '<br>'
+// 				+ item.longitude + '</p>'
+
+// 				output.append(landmark)
+// 			})
+// 		},
+// 		error: function() {
+// 			output.text('There was an error loading the data.')
+// 		}
+// 	})
+
+// 	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
+// 	$("h3").html("HELLO ALIENS")
+// }, false)
 
 
 
