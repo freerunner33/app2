e21b0eefade23fc42cb6e15922b276a6ea5aeba7
Noah Thomas <noahbthomas@sandiego.edu>
2015-08-25 11:20:37 -0700

    Added an alert to test js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('deviceready', function() {
+	alert("Document is ready")
 	var output = $('#output')
 
 	$.ajax({
@@ -21,18 +22,20 @@ document.addEventListener('deviceready', function() {
 	})
 
 	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
+	$("h3").html("HELLO ALIENS")
 }, false)
 
 
 
 // $(document).ready(function() {
-// 	var output = $('#output')
+// 	alert("Document is ready")
+// 	var output = $('div#output')
 
 // 	$.ajax({
 // 		url: 'http://noahthomas.us/pg/landmarks.php',
 // 		dataType: 'jsonp',
 // 		jsonp: 'jsoncallback',
-// 		timeout: 5000,
+// 		timeout: 0,
 // 		success: function(data, status) {
 // 			$.each(data, function(i, item) {
 // 				var landmark = '<h1>' + item.name + '</h1>'
@@ -48,4 +51,5 @@ document.addEventListener('deviceready', function() {
 // 	})
 
 // 	$('#output').html("<p>Hi this is a lovely paragraph testing if the app is even working</p>")
+// 	$("h3").html("HELLO ALIENS")
 // })
