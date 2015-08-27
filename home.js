fb19053458ce3836daae80eed045af0cf2840acb
--unset <--unset>
2015-08-27 16:55:21 -0700

    Commented a ton

--- home.js
+++ home.js
@@ -79,29 +79,29 @@ document.addEventListener('deviceready', function() {
 
 			// 	output.append(menuitem)
 			// })
-
+			str = '<h4>FAIL</h4>'
 			$.each(categories, function(i, cat) {
-				var str = '<div id="collapsible"><h3>' + cat.name + '</h3><div id="content"><table><tr>'
-				str += '<th>Name</th>'
-				str += (cat.size1) ? '<th>' + cat.size1 + '</th>' : ''
-				str += (cat.size2) ? '<th>' + cat.size2 + '</th>' : ''
-				str += (cat.size3) ? '<th>' + cat.size3 + '</th>' : ''
-				str += (cat.size4) ? '<th>' + cat.size4 + '</th>' : ''
-				str += (cat.size5) ? '<th>' + cat.size5 + '</th>' : ''
-				str += '</tr>'
+				// var str = '<div id="collapsible"><h3>' + cat.name + '</h3><div id="content"><table><tr>'
+				// str += '<th>Name</th>'
+				// str += (cat.size1) ? '<th>' + cat.size1 + '</th>' : ''
+				// str += (cat.size2) ? '<th>' + cat.size2 + '</th>' : ''
+				// str += (cat.size3) ? '<th>' + cat.size3 + '</th>' : ''
+				// str += (cat.size4) ? '<th>' + cat.size4 + '</th>' : ''
+				// str += (cat.size5) ? '<th>' + cat.size5 + '</th>' : ''
+				// str += '</tr>'
 
-				$.each(menu, function(j, item) {
-					if (item.category != cat.name)
-						continue
-					str += '<tr><td>' + item.name + '</td>'
-					str += (item.price1) ? '<td>' + item.price1 + '</td>' : ''
-					str += (item.price2) ? '<td>' + item.price2 + '</td>' : ''
-					str += (item.price3) ? '<td>' + item.price3 + '</td>' : ''
-					str += (item.price4) ? '<td>' + item.price4 + '</td>' : ''
-					str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
-					str += '</tr>'
-				})
-				str += '</table></div></div>'
+				// $.each(menu, function(j, item) {
+				// 	if (item.category != cat.name)
+				// 		continue
+				// 	str += '<tr><td>' + item.name + '</td>'
+				// 	str += (item.price1) ? '<td>' + item.price1 + '</td>' : ''
+				// 	str += (item.price2) ? '<td>' + item.price2 + '</td>' : ''
+				// 	str += (item.price3) ? '<td>' + item.price3 + '</td>' : ''
+				// 	str += (item.price4) ? '<td>' + item.price4 + '</td>' : ''
+				// 	str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
+				// 	str += '</tr>'
+				// })
+				// str += '</table></div></div>'
 
 				output.append(str)
 			})
