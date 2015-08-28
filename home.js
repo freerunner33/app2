66fc6faf7fd938824fa7e336b0c649d8549821e6
--unset <--unset>
2015-08-27 17:23:05 -0700

    IT WORKS, just gotta add this code to phonegap app

--- home.js
+++ home.js
@@ -46,39 +46,8 @@ document.addEventListener('deviceready', function() {
 				'internet connection for up-to-date dining information.')
 		},
 		complete: function() {
-			output.append('<hr><h1>Menu Categories</h1>')
+			output.append('<hr><h1>Menu</h1>')
 
-			// $.each(categories, function(i, cat) {
-			// 	var categoryitem = '<hr><h2>' + cat.name + '</h2><p>'
-			// 		categoryitem += (cat.size1) ? cat.size1 + '<br>' : ''
-			// 		categoryitem += (cat.size2) ? cat.size2 + '<br>' : ''
-			// 		categoryitem += (cat.size3) ? cat.size3 + '<br>' : ''
-			// 		categoryitem += (cat.size4) ? cat.size4 + '<br>' : ''
-			// 		categoryitem += (cat.size5) ? cat.size5 : ''
-			// 		categoryitem += '</p>'
-
-			// 		output.append(categoryitem)
-			// })
-			
-			// output.append('<hr><h1>Menu Items</h1>')
-
-			// $.each(menu, function(i, item) { 	// assigns menu[i] to item
-				
-			// 	var menuitem = ''
-			// 	if (category != item.category) {	// if new category then label it
-			// 		category = item.category
-			// 		menuitem += '<hr><h1>' + category + '</h1>'
-			// 	}
-			// 	menuitem += '<h3>' + item.name + '</h3><p>'
-			// 	menuitem += (item.price1) ? item.price1 + '<br>' : ''
-			// 	menuitem += (item.price2) ? item.price2 + '<br>' : ''
-			// 	menuitem += (item.price3) ? item.price3 + '<br>' : ''
-			// 	menuitem += (item.price4) ? item.price4 + '<br>' : ''
-			// 	menuitem += (item.price5) ? item.price5 : ''
-			// 	menuitem += '</p>'
-
-			// 	output.append(menuitem)
-			// })
 			$.each(categories, function(i, cat) {
 				var str = '<div id="collapsible"><h3>' + cat.name + '</h3><div id="content">'
 				str += '<table><tr>'
@@ -100,11 +69,6 @@ document.addEventListener('deviceready', function() {
 						str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
 						str += '</tr>'
 					}
-					// 	str += (item.price1) ? '<td>' + item.price1 + '</td>' : ''
-					// 	str += (item.price2) ? '<td>' + item.price2 + '</td>' : ''
-					// 	str += (item.price3) ? '<td>' + item.price3 + '</td>' : ''
-					// 	str += (item.price4) ? '<td>' + item.price4 + '</td>' : ''
-					// 	str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
 				})
 				
 				str += '</table>'
